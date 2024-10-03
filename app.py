41be788b7837b9e71581451d201f28f66574f44c
ovikboss <ovikboss2016@gmail.com>
2024-10-04 00:28:07 +0300

    develop

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+app.config['WTF_CSRF_ENABLED'] = False
 
-if __name__ == 'main':
+@app.route('/main')
+def hello(name = None):
+  if name != None:
+    return render_template('main.html', name=name)
+  else:
+     return "вы не авторизованы"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form["username"]
+        return hello(name)
+    return render_template('reg.html')
+
+    
+
+
+
+if __name__ == '__main__':
   app.run(debug=True, port=5000)
